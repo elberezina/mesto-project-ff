250bbf6de1719273167490c55dc139db7e70c29e
Eleonora Berezina <elbrz@yandex.ru>
2023-12-02 18:25:36 +0300

    second version

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,37 +1,25 @@
-
 function openModal(popup) {
-    popup.classList.add('popup_is-opened', 'popup_is-animated');
-    document.addEventListener('keydown', handleCloseModalEsc);
-    document.addEventListener('click', handleCloseModalOverlay); 
-    document.addEventListener('click', handleCloseModalButton); 
+  popup.classList.add('popup_is-opened', 'popup_is-animated');
+  document.addEventListener('keydown', handleCloseModalEsc);
 }
 
 function closeModal(popup) {
-    popup.classList.remove('popup_is-opened');
-    document.removeEventListener('keydown', handleCloseModalEsc);
-    document.removeEventListener('click', handleCloseModalOverlay);
-    document.removeEventListener('click', handleCloseModalButton); 
+  popup.classList.remove('popup_is-opened');
+  document.removeEventListener('keydown', handleCloseModalEsc);
 }
 
-function handleCloseModalButton()  {
-    const modalOpened = document.querySelector('.popup_is-opened');
-    const closeModalButton = modalOpened.querySelector('.popup__close');
-    closeModalButton.addEventListener("click", () => 
-    closeModal(modalOpened))
-}
-
-function handleCloseModalOverlay(evt)  {
-    if (evt.target.classList.contains('popup_is-opened')) {
+function handleCloseModalOverlay(evt) {
+  if (evt.target.classList.contains('popup_is-opened')) {
     const modalOpened = document.querySelector('.popup_is-opened');
     closeModal(modalOpened);
-    }
+  }
 }
 
 function handleCloseModalEsc(evt) {
-    if (evt.key==='Escape') {
-      const modalOpened = document.querySelector('.popup_is-opened');
-      closeModal(modalOpened);
-    }
+  if (evt.key === 'Escape') {
+    const modalOpened = document.querySelector('.popup_is-opened');
+    closeModal(modalOpened);
+  }
 }
 
-export { openModal, closeModal };
+export { openModal, closeModal, handleCloseModalOverlay };
--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import {
   handleDeleteCard,
   handleLikeCard,
 } from './components/card.js';
-import { openModal, closeModal } from './components/modal.js';
+import {
+  openModal,
+  closeModal,
+  handleCloseModalOverlay,
+} from './components/modal.js';
 
 export const cardTemplate = document.querySelector('#card-template').content;
-const cardsList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
-const newCardPButton = document.querySelector('.profile__add-button');
+const newCardButton = document.querySelector('.profile__add-button');
 const newCardModal = document.querySelector('.popup_type_new-card');
 const newCardNameInput = document.querySelector('.popup__input_type_card-name');
 const newCardLinkInput = document.querySelector('.popup__input_type_url');
@@ -20,60 +24,77 @@ const cardModal = document.querySelector('.popup_type_image');
 const imageCardModal = cardModal.querySelector('.popup__image');
 const captionCardModal = cardModal.querySelector('.popup__caption');
 
-const formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_description');
 
-const profilePopup = document.querySelector('.popup_type_edit');
+const profileModal = document.querySelector('.popup_type_edit');
+const formElementProfile = profileModal.querySelector('.popup__form');
 const profileButton = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__description');
 
+const closeProfileModalButton = profileModal.querySelector('.popup__close');
+const closeNewCardModalButton = newCardModal.querySelector('.popup__close');
+const closeCardModalButton = cardModal.querySelector('.popup__close');
+const popupList = [profileModal, newCardModal, cardModal];
+
 function addCards() {
   initialCards.forEach(function (card) {
-    cardsList.append(
+    cardsContainer.append(
       createCard(card, handleDeleteCard, handleLikeCard, showCardInModal)
     );
   });
 }
 
-profileButton.addEventListener('click', function () {
-  openModal(profilePopup);
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-});
-
-newCardPButton.addEventListener('click', function () {
-  openModal(newCardModal);
-});
-
-function showCardInModal(img) {
-  imageCardModal.src = img.target.src;
-  imageCardModal.alt = img.target.alt;
-  captionCardModal.textContent = img.target.alt;
+function showCardInModal(card) {
+  imageCardModal.src = card.link;
+  imageCardModal.alt = card.name;
+  captionCardModal.textContent = card.name;
   openModal(cardModal);
 }
 
-function handleFormSubmit(evt) {
+function handleProfileSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
+  closeModal(profileModal);
 }
-formElement.addEventListener('submit', handleFormSubmit);
 
 function handleNewCardSubmit(evt) {
   evt.preventDefault();
   const newCard = {
     name: newCardNameInput.value,
     link: newCardLinkInput.value,
-    alt: newCardNameInput.value,
   };
-  cardsList.prepend(
+  cardsContainer.prepend(
     createCard(newCard, handleDeleteCard, handleLikeCard, showCardInModal)
   );
   closeModal(newCardModal);
   evt.target.reset();
 }
+
+function profileModalForm() {
+  openModal(profileModal);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+popupList.forEach((popup) => {
+  popup.addEventListener('click', handleCloseModalOverlay);
+});
+
+newCardButton.addEventListener('click', function () {
+  openModal(newCardModal);
+});
+profileButton.addEventListener('click', profileModalForm);
+formElementProfile.addEventListener('submit', handleProfileSubmit);
 formElementCard.addEventListener('submit', handleNewCardSubmit);
+closeProfileModalButton.addEventListener('click', () =>
+  closeModal(profileModal)
+);
+closeNewCardModalButton.addEventListener('click', () =>
+  closeModal(newCardModal)
+);
+closeCardModalButton.addEventListener('click', () => closeModal(cardModal));
 
 addCards();
